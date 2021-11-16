cc6c9e300f323a64b1f9a20159201a24cfb32b19
Jean-LoupMARTIN <jean_loup_martin@hotmail.com>
2021-11-16 12:51:24 +0100

    delta pitch particle

--- Assets/Projectile/Script/Particle.cs
+++ Assets/Projectile/Script/Particle.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem), typeof(AudioSource))]
 public class Particle : MonoBehaviour
 {
+    [Range(0, 3)]
+    public float pitch = 1;
+    public float dpitch = 0.15f;
+
     ParticleSystem particle;
     AudioSource sound;
     public float shake = 0;
+
+
 
     private void Awake()
     {
@@ -18,6 +24,11 @@ public class Particle : MonoBehaviour
     private void OnEnable()
     {
         if (shake > 0) Tool.ShakeSphere(transform.position, shake);
+    }
+
+    private void OnDisable()
+    {
+        sound.pitch = pitch + Random.Range(-dpitch, dpitch);
     }
 
     private void Update()
